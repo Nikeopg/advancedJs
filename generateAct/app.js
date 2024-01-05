da022bbb9ca7921c980f5dadbb6383415a3b9b3f
Nikita <vns95@bk.ru>
2024-01-06 01:23:27 +0300

    upd 06/01

---
+++ generateAct/app.js
@@ -0,0 +1,30 @@
+"use strict"
+
+const wrapper = document.querySelector('.wrapper');
+
+
+async function getActivity() {
+    const res = await fetch('https://www.boredapi.com/api/activity/')
+    return res.json();
+}
+
+
+async function generate() {
+    try {
+        wrapper.innerHTML = '';
+        const data = await Promise.all([
+            getActivity(),
+            getActivity(),
+            getActivity()]);
+        console.log(data)
+
+        for (const el of data) {
+            const element = document.createElement('div');
+            element.innerHTML = `${el.activity}`;
+            wrapper.appendChild(element);
+        }
+    }
+    catch (e) {
+        console.error(e)
+    }
+}
